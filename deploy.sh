7642dd00c4a24a37991aabbdde6ab900e87932a7
MohammadReza Ebrahimi <mamareza.e@gmail.com>
2022-05-13 13:53:55 -0400

    bash style instead of PS

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-$AWSID="123456789012"
-$REGION="ca-central-1"
-$IMAGE="lambda-telegram-macbook"
+AWSID="123456789012"
+REGION="ca-central-1"
+IMAGE="lambda-telegram-macbook"
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $AWSID.dkr.ecr.$REGION.amazonaws.com    
 aws ecr create-repository --repository-name $IMAGE --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
